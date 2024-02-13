e600e8d724230bc9b6da3cf33e65360c72f1b2da
Kris Visaya <131139444+bojji3@users.noreply.github.com>
2024-02-13 08:30:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ function drawText() {
     //fades out the text by decreasing the opacity
     if(frameNumber >= 250 && frameNumber < 500){
         context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
-        context.fillText("everyday day I cannot believe how lucky I am", canvas.width/2, canvas.height/2);
+        context.fillText("to know a person such as yourself will always be a blessing and a wonder", canvas.width/2, canvas.height/2);
         opacity = opacity - 0.01;
     }
 
